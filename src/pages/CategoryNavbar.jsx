1227c8b9b57c5ed81d0c602690b6b2348fbb4496
Bekim Burimi <107232029+Coderkidd99@users.noreply.github.com>
2023-04-27 14:11:49 +0700

    changed bg color for sub menu

--- src/pages/CategoryNavbar.jsx
+++ src/pages/CategoryNavbar.jsx
@@ -1,14 +1,136 @@
+import { useState } from 'react';
 
+function CategoryNavbar() {
+  const [menuState, setMenuState] = useState({
+    item1: false,
+    item2: false,
+    item3: false,
+    item4: false,
+  });
 
-const CategoryNavbar = () => {
+  const toggleMenu = (item) => {
+    setMenuState((prevState) => ({ ...prevState, [item]: !prevState[item] }));
+  };
   return (
-    <div className="">
-        <div className="">
-            <span className="">Home</span>
-
-        </div>
-    </div>
-  )
+    <nav className="w-full text-black py-3">
+      <div className="mx-auto container flex justify-center items-center">
+        <ul className="flex">
+          <li className="relative mx-4">
+            <a
+              href="#"
+              className="hover:text-gray-400"
+              onMouseEnter={() => toggleMenu('item1')}
+              onMouseLeave={() => toggleMenu('item1')}
+            >
+              Clothing
+              {menuState.item1 && (
+                <ul className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-gray-300 rounded-md p-2">
+                  <li className="mb-2">
+                    <a href="/mensclothing" className="hover:text-gray-400">
+                      Men's Clothing
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-gray-400">
+                      Women's Clothing
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </a>
+          </li>
+          <li className="relative mx-4">
+            <a
+              href="#"
+              className="hover:text-gray-400"
+              onMouseEnter={() => toggleMenu('item2')}
+              onMouseLeave={() => toggleMenu('item2')}
+            >
+              Jewellery
+              {menuState.item2 && (
+                <ul className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-gray-300 rounded-md p-2">
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 1
+                    </a>
+                  </li>
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 3
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </a>
+          </li>
+          <li className="relative mx-4">
+            <a
+              href="#"
+              className="hover:text-gray-400"
+              onMouseEnter={() => toggleMenu('item3')}
+              onMouseLeave={() => toggleMenu('item3')}
+            >
+              Electronics
+              {menuState.item3 && (
+                <ul className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-gray-300 rounded-md p-2">
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 1
+                    </a>
+                  </li>
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 3
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </a>
+          </li>
+          <li className="relative mx-4">
+            <a
+              href="#"
+              className="hover:text-gray-400"
+              onMouseEnter={() => toggleMenu('item4')}
+              onMouseLeave={() => toggleMenu('item4')}
+            >
+              Examples
+              {menuState.item4 && (
+                <ul className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-gray-300 rounded-md p-2">
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 1
+                    </a>
+                  </li>
+                  <li className="mb-2">
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-gray-400">
+                      Submenu Item 3
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default CategoryNavbar
+export default CategoryNavbar;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,47 @@
+import CategoryNavbar from "./CategoryNavbar";
+
 const Home = () => {
   return (
-    <div className="px-24">
-      <div className="flex ">
-        <h1 className=" text-zinc-900  p-4 text-3xl font-bold ml-20 mt-11 mb-8 ">
-          Shop our popular categories
-        </h1>
-      </div>
-      <div className="flex items-center justify-start ml-20 gap-11 flex-wrap">
-        <div className="relative rounded-full overflow-hidden w-48 h-48">
-          <img
-            className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-            src="path/to/your/image.jpg"
-            alt="Electronics"
-          />
+    <div>
+      <CategoryNavbar />
+      <div className="px-24">
+        <div className="flex ">
+          <h1 className=" text-zinc-900  p-4 text-3xl font-bold ml-20 mt-11 mb-8 ">
+            Shop our popular categories
+          </h1>
         </div>
+        <div className="flex items-center justify-start ml-20 gap-11 flex-wrap">
+          <div className="relative rounded-full overflow-hidden w-48 h-48">
+            <img
+              className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+              src="path/to/your/image.jpg"
+              alt="Electronics"
+            />
+          </div>
 
-        <div className="relative rounded-full overflow-hidden w-48 h-48">
-          <img
-            className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-            src="path/to/your/image.jpg"
-            alt="Jewelry"
-          />
-        </div>
+          <div className="relative rounded-full overflow-hidden w-48 h-48">
+            <img
+              className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+              src="path/to/your/image.jpg"
+              alt="Jewelry"
+            />
+          </div>
 
-        <div className="relative rounded-full overflow-hidden w-48 h-48">
-          <img
-            className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-            src="path/to/your/image.jpg"
-            alt="Men's Clothing"
-          />
-        </div>
+          <div className="relative rounded-full overflow-hidden w-48 h-48">
+            <img
+              className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+              src="path/to/your/image.jpg"
+              alt="Men's Clothing"
+            />
+          </div>
 
-        <div className="relative rounded-full overflow-hidden w-48 h-48">
-          <img
-            className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-            src="path/to/your/image.jpg"
-            alt="Women's Clothing"
-          />
+          <div className="relative rounded-full overflow-hidden w-48 h-48">
+            <img
+              className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+              src="path/to/your/image.jpg"
+              alt="Women's Clothing"
+            />
+          </div>
         </div>
       </div>
     </div>
