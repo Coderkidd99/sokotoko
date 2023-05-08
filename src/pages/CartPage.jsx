a39c13b80fe70edca3ccbcd3de86cd554ac29682
Bekim Burimi <107232029+Coderkidd99@users.noreply.github.com>
2023-05-08 19:41:21 +0700

    update

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "./CartContext";
+import  CartContext  from "./CartContext";
 
 const CartPage = () => {
   const { items, cartTotal, removeFromCart } = useContext(CartContext);
