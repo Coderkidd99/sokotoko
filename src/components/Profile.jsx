20d0d6cc732c9c84f0298371980b9c9ae3a8e538
Bekim Burimi <107232029+Coderkidd99@users.noreply.github.com>
2023-05-23 17:41:34 +0700

    updated a few minr things

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, error } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
   return (
     isAuthenticated && (
-      <div className="flex ">
-        <img src={user.picture} alt={user.name} className=""/>
-        <p className=" text-sm font-normal">{user.email}</p>
+      <div className="flex">
+        <img src={user.picture} alt={user.name} className="" />
+        <p className="text-sm font-normal">{user.email}</p>
       </div>
     )
   );
